557b9774a3fe9fa31d43fcca6176b4bbeb46c3f8
Ankitpal23 <palprinshu28@gmail.com>
2024-04-01 20:56:47 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,41 @@
+const questions = [
+    {
+      text: "Which of the following is used in React.js to increase performance?",
+      options: [
+        { id: 0, opt: "a", text: "Virtual DOM", isCorrect: true },
+        { id: 1, opt: "b", text: "Original DOM", isCorrect: false },
+        { id: 2, opt: "c", text: "Both Aand B", isCorrect: false },
+        { id: 3, opt: "d", text: "None of the Above", isCorrect: false }
+      ]
+    },
+    {
+      text: "Which of the following is not a disadvantage of React.js?",
+      options: [
+        { id: 0, opt: "a", text: "React.js has only a view layer. We have put your code for Ajax requests, events and so on.", isCorrect: false },
+        { id: 1, opt: "b", text: "The JSX in React.js makes code easy to read and write.", isCorrect: true },
+        { id: 2, opt: "c", text: "The library of React.js is pretty large.", isCorrect: false },
+        { id: 3, opt: "d", text: "The learning curve can be steep in React.js.", isCorrect: false }
+      ]
+    },
+    {
+      text: "A class is a type of function, but instead of using the keyword function to initiate it, which keyword do we use?",
+      options: [
+        { id: 0, opt: "a", text: "Constructor", isCorrect: false },
+        { id: 1, opt: "b", text: "Object", isCorrect: false },
+        { id: 2, opt: "c", text: "Class", isCorrect: true },
+        { id: 3, opt: "d", text: "DataObject", isCorrect: false }
+      ]
+    },
+    {
+      text: "Which of the following acts as the input of a class-based component?",
+      options: [
+        { id: 0, opt: "a", text: "Class", isCorrect: false },
+        { id: 1, opt: "b", text: "Factory", isCorrect: false },
+        { id: 2, opt: "c", text: "Props", isCorrect: true },
+        { id: 3, opt: "d", text: "Render", isCorrect: false }
+      ]
+    }
+
+  ];
+
+export default questions;  
